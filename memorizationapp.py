bf0638ffc0a21a0c97923745cdccd99c360db85c
Santiago Norena <san.norena@gmail.com>
2019-08-14 06:18:20 -0400

    Update memorizationapp.py
    update functions and code design

--- memorizationapp.py
+++ memorizationapp.py
@@ -1,15 +1,26 @@
-#---------Main objective:----------
+#-----------------------------NOTES:------------------------------------
+# - terminal shortcut: Documents/Github/memorization-app
+#-----------------------MAINE OBJECTIVES:-------------------------------
 #  - Just do a simple demonstration that it will work to practice skills
 #  - no need to include GUI
 #  - Keep it simple for now!!!
-
-#--------------To do:-------------
-# 1) do research in OOB (object oriented programming) for python / read the articles - watch videos
-#      - read the articles / watch videos 
+#-----------------------------TODO:-------------------------------------
+# 1) do research in OOB (object oriented programming)
+#     - read the articles / watch videos 
 # 2) look at similar projects online
-# 3) read to learn to connect to database: http://www.postgresqltutorial.com/postgresql-python/connect/
+# 3) read to learn to connect to a database:
+#     - http://www.postgresqltutorial.com/postgresql-python/connect/
 
 topic =[]
+
+def main():
+    print("What question would you like to save?")
+    question = input().strip()
+    print("What is the answer for the question?")
+    answer = input().strip()
+
+    SaveQuestion(question, answer)
+
 
 def SaveQuestion(question, answer):
     
@@ -20,7 +31,8 @@ def SaveQuestion(question, answer):
     print("Would you like to add another question?")
     response = input().strip()
     SaveAnotherQuestion(response, topic)
-    
+
+
 def SaveAnotherQuestion(response, topic):
     if response == 'y' or 'yes':
         print("What question would you like to save?")
@@ -30,19 +42,12 @@ def SaveAnotherQuestion(response, topic):
 
         SaveQuestion(question, answer)
 
-    elif response == 'n' or 'no': # ERROR: Something is not working here
+    elif response == 'n' or 'no': # ERROR: not working for this case!!! 
         print(topic)
     else:
-        print("Enter yes or no")
+        print("Enter yes or no")     
+        print("yes")
 
-
+        
 if __name__ == '__main__':
-
-    print("What question would you like to save?")
-    question = input().strip()
-
-    print("What is the answer for the question?")
-    answer = input().strip()
-
-    SaveQuestion(question, answer)
-
+    main()
