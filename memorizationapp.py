4de30afe662b0d3ed8dbe1c1050282ccb1ae78ad
Santiago Norena <san.norena@gmail.com>
2019-08-14 09:30:45 -0400

    Update memorizationapp.py

--- memorizationapp.py
+++ memorizationapp.py
@@ -28,24 +28,24 @@ def SaveQuestion(question, answer):
     topic.append(new) 
     print(topic)
 
+    result = SaveAnotherQuestion(topic)
+    print(result)
+
+def SaveAnotherQuestion(topic):
     print("Would you like to add another question?")
     response = input().strip()
-    SaveAnotherQuestion(response, topic)
-
-
-def SaveAnotherQuestion(response, topic):
-    if response == 'y' or 'yes':
+    if (response == "yes" or "y"):
         print("What question would you like to save?")
         question = input().strip()
         print("What is the answer for the question?")
         answer = input().strip()
 
         SaveQuestion(question, answer)
-
-    elif response == 'n' or 'no': # ERROR: not working for this case!!! 
-        print(topic)
+        
+    elif (response == "no" or "n"): # ERROR: not working for this case!!! 
+        return print(topic)
     else:
-        print("Enter yes or no")     
+        return print("Enter yes or no")     
 
      
 if __name__ == '__main__':
