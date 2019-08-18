1c4e8f7a7f96d347cdd97b280c16f4d032c2313a
Santiago Norena <san.norena@gmail.com>
2019-08-17 20:55:27 -0400

    .txt database functionality
    - Added functions to save data to .txt file
    - Added option to save more questions

--- memorizationapp.py
+++ memorizationapp.py
@@ -5,20 +5,21 @@
 #  - Just do a simple demonstration that it will work to practice skills
 #  - no need to include GUI yet
 #  - Keep it simple for now!!!
-#-----------------------------TODO:-------------------------------------
-# 1) look at similar projects to online to spark ideas - (DONE)
-# 2) Implement extracting questions & answeres from .txt file to use as a database - (DONE)
-# 3) Implement saving questions & answers in the .txt file
-# 4) Implement global variables functionality
+#---------------------------BACKLOG:------------------------------------
+# - look at similar projects to online to spark ideas - (DONE)
+# - Implement extracting questions & answers from .txt file to use as a database - (DONE)
+# - Implement saving questions & answers in the .txt file - (DONE)
+# - Improve functionality to save another variable
+# - Improve global variables functionality
+# - 
 
 cards =[]
 
 def main():
-    cards =[]
     LoadFile(cards)
-
+    SaveQuestion()
     '''with open("topicname.txt", "r") as dfile:
-        #for line in iter(dfile.readline, ''):
+        #for line in iter(5dfile.readline, ''):
         for line in dfile:
             temp = eval(line)
             cards.append(temp)
@@ -39,19 +40,34 @@ def LoadFile(cards):
         for line in dfile:
             temp = eval(line)
             cards.append(temp)
-            print(cards) 
-'''
-def SaveQuestion(question, answer):
-    
+        print(cards) 
+
+def SaveQuestion():
+    print("Type the 'Question': ")
+    question = input().strip()
+    print("Type the 'Answer': ")
+    answer = input().strip()
     new = [question,answer]
     cards.append(new) 
     print(cards)
 
-    result = SaveAnotherQuestion(cards)
-    print(result)
+    with open("topicname.txt", "w") as dfile:
+        for line in cards:
+            # TODO: add function so it doesnt rewrite entire file
+            dfile.write("%s\n" % line)
+    
+    print("Would you like save another question?")
+    response = input().strip()
+    if response == "yes":
+        SaveAnotherQuestion()
+    else:
+        pass
 
-def SaveAnotherQuestion(cards):
-    print("Would you like to add another question?")
+def SaveAnotherQuestion():
+    SaveQuestion()
+    
+    
+"""     print("Would you like to add another question?")
     response = input().strip()
     if (response == "yes" or "y"):
         print("What question would you like to save?")
@@ -64,8 +80,8 @@ def SaveAnotherQuestion(cards):
     elif (response == "no" or "n"): #ERROR! not working
         return print(cards)
     else:
-        return print("Enter yes or no")     
-'''
-     
+        return print("Enter yes or no")     """ 
+
+
 if __name__ == '__main__':
     main()
